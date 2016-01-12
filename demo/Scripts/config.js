1bdaeccddf015770fe96e1cb1112438b78b5dca3
Zyshchyk Maksim <amporioo@gmail.com>
2016-01-12 11:53:46 +0300

    Add Knockout-Localization project

---
+++ demo/Scripts/config.js
@@ -0,0 +1,27 @@
+﻿'use strict';
+
+requirejs.config({
+	baseUrl: '/Scripts/',
+	urlArgs: "v=" + (new Date()).getTime(),
+	shim: {
+		'bootstrap': {
+			deps: ['jquery']
+		}
+	},
+
+	paths: {
+		jquery: 'jquery-2.1.4',
+		bootstrap: 'bootstrap.min',
+		knockout: 'knockout-3.3.0'
+	}
+
+});
+
+requirejs.onError = function (err) {
+	console.log(err.requireType);
+	if (err.requireType === 'timeout') {
+		console.log('modules: ' + err.requireModules);
+	}
+
+	console.log(err);
+};
---
+++ demo/Scripts/modules/localization.js
@@ -0,0 +1,90 @@
+﻿define(['jquery', 'knockout', 'bootstrap'], function ($, ko) {
+
+	(function () {
+
+		function getLocalizedText(objLanguage, params) {
+
+			var key = params.key;
+			var strFormat = params.strFormat;
+
+			var item = objLanguage[key];
+
+			if (!item) {
+				return 'Not found the translation for the key ' + key;
+			}
+
+			var text = item;
+
+			if (strFormat) {
+				for (var replaceKey in strFormat) {
+					var replacement = strFormat[replaceKey];
+					text = text.replace(replaceKey, replacement);
+				}
+			}
+
+			return text;
+		}
+
+
+		ko.bindingHandlers.localization = {
+			update: function (element, valueAccessor, allBindingsAccessor, viewModel, context) {
+
+				var params = allBindingsAccessor().params;
+				var binding = ko.utils.unwrapObservable(valueAccessor());
+
+				if (typeof binding !== "undefined" && typeof params !== "undefined") {
+					var text = getLocalizedText(binding, params);
+
+					if (element.localName.toLowerCase() === 'input' && element.type.toLowerCase() === 'text') {
+						element.value = text;
+					} else {
+						ko.bindingHandlers.html.update(
+							element,
+							function () { return text },
+							allBindingsAccessor,
+							viewModel,
+							context);
+					}
+				} else {
+					console.log('binding object is not defined');
+				}
+
+			}
+		};
+
+	})();
+
+
+	return function localizationViewModel() {
+
+		self.language = ko.observable();
+
+		function getLocalization(language) {
+			switch (language) {
+				case 'de':
+					self.language({ HelloKey: "Hallo" });
+					break;
+				case 'en':
+					self.language({ HelloKey: "Hello" });
+					break;
+				case 'ru':
+					self.language({ HelloKey: "Привет" });
+					break;
+				default:
+					self.language({ HelloKey: "Hello" });
+					break;
+			}
+		}
+
+		self.choosede = function () {
+			getLocalization('de');
+		};
+		self.chooseen = function () {
+			getLocalization('en');
+		};
+
+		self.chooseru = function () {
+			getLocalization('ru');
+		};
+	}
+});
